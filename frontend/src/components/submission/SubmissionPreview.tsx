03bb6072d4c9f34bb3090175988a9b5d94727938
Daftpy <daftpy@gmail.com>
2023-09-24 01:12:29 -0500

    Refactor: Altered SubmissionPreview layout.

--- frontend/src/components/submission/SubmissionPreview.tsx
+++ frontend/src/components/submission/SubmissionPreview.tsx
@@ -20,29 +20,52 @@ export function SubmissionPreview({ submission }: IPreviewProps) {
   return (
     <div className={styles.preview}>
       <div className={styles.previewContent}>
-        <div>
-          <h3>
-            <Link
-              className={styles.link}
-              to={`/submission/${submission.commentId}/`}
-            >
-              {submission.title}
-            </Link>
-          </h3>
-          <div className={styles.meta}>
-            <div className={styles.author}>Author</div>
-            <div>
-              {formatDistanceToNow(new Date(submission.createdAt), {
-                addSuffix: true,
-              })}
+        <div className={styles.header}>
+          <div>
+            <h3>
+              <Link
+                className={styles.link}
+                to={`/submission/${submission.commentId}/`}
+              >
+                {submission.title}
+              </Link>
+            </h3>
+            <div className={styles.meta}>
+              <div className={styles.author}>Author</div>
+              <div>
+                {formatDistanceToNow(new Date(submission.createdAt), {
+                  addSuffix: true,
+                })}
+              </div>
+              <div className={styles.fileType}>
+                {submission.type == "FILE" ? (
+                  <>
+                    File
+                    <span className={`material-symbols-outlined`}>
+                      stock_media
+                    </span>
+                  </>
+                ) : (
+                  <>
+                    Link
+                    <span
+                      className={`material-symbols-outlined ${styles.tilted}`}
+                    >
+                      link
+                    </span>
+                  </>
+                )}
+              </div>
             </div>
           </div>
         </div>
         <p className={styles.description}>{submission.description}</p>
       </div>
       <div className={styles.previewMeta}>
-        <div>{submission.type}</div>
-        {/*<div>Replies: {submission.replyCount}</div>*/}
+        <div>
+          <span className="material-symbols-outlined">comment</span>
+          {submission.replyCount}
+        </div>
       </div>
     </div>
   );
